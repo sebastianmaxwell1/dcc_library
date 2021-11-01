4f9ccd06a3471d7e377799120c679fb6fa56f017
Sebastian Maxwell <sebastianrmaxwell@gmail.com>
2021-11-01 18:19:05 -0400

    Created Previous Book Button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,22 +16,53 @@ class App extends Component {
         };
     }
 
+    goToNextBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber++;
+        if(tempBookNumber === this.books.length){
+            tempBookNumber = 0;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+
+    goToPreviousBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber--;
+        if (tempBookNumber < 0){
+            tempBookNumber = this.books.length - 1
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+
 
     render(){
         return (
             <div className="container-fluid">
                 <TitleBar />
-                <div className="col-md-4"></div>
-                {/* Button for previous */}
-                <div className="col-md-4"></div>
-                {/* Book Display */}
-                    <h1>{this.books[this.state.bookNumber].title}</h1>
-                    <h4>{this.books[this.state.bookNumber].author}</h4>
-                <div className="col-md-4"></div>
-                {/* Button for Next Book */}
+                <div className= "row">
+                    <div className="col-md-4">
+                    <button onClick={this.goToPreviousBook}>Previous Book</button>
+                    </div>
+                    {/* Button for previous */}
+                    <div className="col-md-4">
+                    {/* Book Display */}
+                        <h1>{this.books[this.state.bookNumber].title}</h1>
+                        <h4>{this.books[this.state.bookNumber].author}</h4>
+                    </div>
+                    <div className="col-md-4">
+                        {/* Button for Next Book */}
+                        <button onClick={this.goToNextBook}>Next Book</button>
+                        </div>
+                    </div>
             </div>
         )
     }
+
+
 }
 
 export default App;

43c8b898bfc0b301c6119a72a4ffba05d0019ad7
Sebastian Maxwell <sebastianrmaxwell@gmail.com>
2021-11-01 17:10:57 -0400

    Add Title Bar

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import react,{Component} from "react";
+import './App.css';
+import TitleBar  from "./TitleBar/TitleBar";
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+
+    render(){
+        return (
+            <div className="container-fluid">
+                <TitleBar />
+            </div>
+        )
+    }
+}
+
+export default App;

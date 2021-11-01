2477b12f0359ef01f0b22f03ef6a2d96dfca77af
Sebastian Maxwell <sebastianrmaxwell@gmail.com>
2021-11-01 17:22:33 -0400

    Added books

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,15 @@ import TitleBar  from "./TitleBar/TitleBar";
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = {};
+        this.books = [
+            {title: "The 50th Law", author: "50 Cent & Robert Greene"},
+            {title: "The Hobbit", author: "J.R.R Tolkein"},
+            {title: "The Lord of the Rings", author: "J.R.R Tolkein"},
+
+        ];
+        this.state = {
+            bookNumber: 0
+        };
     }
 
 
@@ -13,6 +21,14 @@ class App extends Component {
         return (
             <div className="container-fluid">
                 <TitleBar />
+                <div className="col-md-4"></div>
+                {/* Button for previous */}
+                <div className="col-md-4"></div>
+                {/* Book Display */}
+                    <h1>{this.books[this.state.bookNumber].title}</h1>
+                    <h4>{this.books[this.state.bookNumber].author}</h4>
+                <div className="col-md-4"></div>
+                {/* Button for Next Book */}
             </div>
         )
     }
